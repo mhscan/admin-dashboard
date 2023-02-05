55381ce3fde868d0a8727542971601df3a6239e9
mhscan <mr.rezaeeian@gmail.com>
2023-02-05 14:32:51 +0330

    creat topbar

--- src/Component/Topbar/Topbar.js
+++ src/Component/Topbar/Topbar.js
@@ -1,14 +1,33 @@
 import React from "react";
+import "./Topbar.css"
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LanguageIcon from '@mui/icons-material/Language';
 
 export default function Topbar() {
   return <div className="topbar">
-topbar
-  
+
+    <div className="top-left">
+      <img className="logo" src="images/logo.png" alt="logo" />
+    </div>
+
+    <div className="top-right">
+      <div className="topbar-icon-container">
+        <NotificationsActiveIcon></NotificationsActiveIcon>
+        <span className="topbar-icon-badge ">2</span>
+      </div>
+      <div className="topbar-icon-container">
+        <SettingsIcon></SettingsIcon>
+        <span className="topbar-icon-badge ">2</span>
+      </div>
+      <div className="topbar-icon-container">
+        <LanguageIcon></LanguageIcon>
+      </div>
+      <img src="images/admin.jpg" alt="admin.png" />
+
+    </div>
 
 
 
-</div>;
+  </div>;
 }
